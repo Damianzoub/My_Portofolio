08bd5e60d3131afb219b5c0d41b636c67d938c47
Damianos Zoumpos <134238559+Damianzoub@users.noreply.github.com>
2025-02-22 17:31:34 +0200

    Update utils.py

--- appfile/utils.py
+++ appfile/utils.py
@@ -2,6 +2,6 @@ from bs4 import BeautifulSoup
 
 def get_project_titles():
     "gets all the h3 header titles projects for later use"
-    with open('/Users/Damian/Desktop/Portofolio/appfile/templates/projects_page.html',encoding='utf-8') as f:
+    with open('appfile/templates/projects_page.html',encoding='utf-8') as f:
         soup = BeautifulSoup(f,'html.parser')
     return [h3.text.strip() for h3 in soup.find_all('h3')]
